8ada316fe5559709099621959c3452b0c79b687b
rexxar <ramiroyuyes@gmail.com>
2026-04-02 19:05:25 -0500

    feat: añadir health check al script de despliegue

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# 1. Definir variables (Buenas prácticas)
 DESTINO="/var/www/html"
 ORIGEN=$(pwd)
 
 echo "🚀 Iniciando despliegue..."
-
-# 2. Simular o ejecutar la copia del archivo
-# Usamos 'sudo' porque /var/www/html es una carpeta protegida
 sudo cp $ORIGEN/index.html $DESTINO/index.html
+echo "✅ Archivos copiados."
 
-echo "✅ Despliegue completado con éxito."
+# --- NUEVA SECCIÓN: HEALTH CHECK ---
+echo "🧐 Verificando la salud del sitio..."
+
+# curl -s (silencioso) -o /dev/null (no queremos ver el HTML) 
+# -w "%{http_code}" (queremos ver el código de estado HTTP, ej. 200)
+ESTADO=$(curl -s -o /dev/null -w "%{http_code}" http://localhost)
+
+if [ $ESTADO -eq 200 ]; then
+    echo "🔥 ¡ÉXITO! El sitio está online (Código 200)."
+else
+    echo "❌ ERROR: El sitio devolvió código $ESTADO. Revisa Nginx."
+    exit 1
+fi
